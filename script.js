9fbfe766d9b38de80eaac88caab535776ae6afd4
gorbachevsergey <gorbachev.sa@icloud.com>
2021-06-21 19:54:26 +0700

    fix

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ let button = document.getElementById('buttImageMove')
 let elem = document.getElementById('img')
 
 button.addEventListener('click', function () {
-    elem.classList.remove('imgOld');
-    elem.classList.add('imgNewMove');
-    elem.style.transition='all 2s ease-in-out';
+    if(elem.classList.contains('imgOld')) {
+        elem.classList.remove('imgOld');
+        elem.classList.add('imgNewMove');
+    } else {
+        elem.classList.remove('imgNewMove');
+        elem.classList.add('imgOldgi');
+    }
 })
+
